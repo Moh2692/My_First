e14b37e02d61bf601fc07ac5d5313597b89e9dff
Mohit Bhartiya <mohit.bhartiya.1993@gmail.com>
2022-01-04 16:38:57 +0100

    first

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,12 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("This is first line");
         System.out.println("Hello Me");
+        System.out.println("finished");
     }
 }
